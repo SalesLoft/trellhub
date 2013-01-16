6e9ed81b86003f540eda3e44510b3cf69357dc00
timdorr <timdorr@timdorr.com>
2013-01-16 00:34:10 -0500

    Basic testing.

---
+++ spec/push_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../trellhub'
+require 'spec_helper'
+
+set :environment, :test
+
+describe 'Trellhub' do
+
+  before(:each) do
+    json_fixture = File.open("spec/fixtures/push.json", "rb")
+    @json = JSON.parse(json_fixture.read)
+  end
+
+  def app
+    Sinatra::Application
+  end
+
+  it "receives a push" do
+    post '/push', :payload => @json
+    last_response.should be_ok
+  end
+end
--- trellhub.rb
+++ trellhub.rb
@@ -1,1 +1,4 @@
 require 'sinatra'
+
+post '/push' do
+end
